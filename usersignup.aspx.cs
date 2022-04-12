0f2f5cb5df5feee5186250c406b306207da35973
saitsabanci <sait.sabanci@icloud.com>
2022-04-12 21:44:39 +0300

    eklenen veriler veritabanına işlendi

--- usersignup.aspx.cs
+++ usersignup.aspx.cs
@@ -23,6 +23,41 @@ namespace kbbtestt
         protected void Button1_Click(object sender, EventArgs e)
         {
             //Response.Write("<script>alert('Testing');</script>");
+
+            try
+            {
+                SqlConnection con = new SqlConnection(strcon);
+                if (con.State == ConnectionState.Closed)
+                {
+                    con.Open();
+                }
+
+                SqlCommand cmd = new SqlCommand("INSERT INTO kullanici" +
+                    "(kullanici_adsoyad,kullanici_dogum,kullanici_tel,kullanici_tc,kullanici_ilce,kullanici_sehir,kullanici_adres,kullanici_adi,kullanici_sifre,hesap_durum) " +
+                    "values(@kullanici_adsoyad,@kullanici_dogum,@kullanici_tel,@kullanici_tc,@kullanici_ilce,@kullanici_sehir,@kullanici_adres,@kullanici_adi,@kullanici_sifre,@hesap_durum)", con);
+
+                cmd.Parameters.AddWithValue("@kullanici_adsoyad", TextBox1.Text.Trim());
+                cmd.Parameters.AddWithValue("@kullanici_dogum", TextBox2.Text.Trim());
+                cmd.Parameters.AddWithValue("@kullanici_tel", TextBox3.Text.Trim());
+                cmd.Parameters.AddWithValue("@kullanici_tc", TextBox4.Text.Trim());
+                cmd.Parameters.AddWithValue("@kullanici_ilce", DropDownList1.SelectedItem.Value);
+                cmd.Parameters.AddWithValue("@kullanici_sehir", DropDownList2.SelectedItem.Value);
+                cmd.Parameters.AddWithValue("@kullanici_adres", TextBox5.Text.Trim());
+                cmd.Parameters.AddWithValue("@kullanici_adi", TextBox8.Text.Trim());
+                cmd.Parameters.AddWithValue("@kullanici_sifre", TextBox9.Text.Trim());
+                cmd.Parameters.AddWithValue("@hesap_durum", "pending");
+
+                cmd.ExecuteNonQuery();
+                con.Close();
+                Response.Write("<script>alert('Başarıyla Kayıt Olundu. Giriş Yapmak İçin Giriş Yap'a Gidin');</script>");
+
+
+            }
+            catch (Exception ex)
+            {
+                Response.Write("<script>alert('" + ex.Message + "');</script>");
+
+            }
         }
     }
 }
